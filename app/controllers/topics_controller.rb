9be11081a0f2ba7e80f84ced485a6e5dcaf9718d
lmariel <laurenmariel90@gmail.com>
2014-05-03 15:45:52 -0400

    order by votes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all
+    @topics = @topics.sort_by { |post| post.votes.size}.reverse
   end
 
   # GET /topics/1
